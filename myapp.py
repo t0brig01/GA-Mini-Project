3982b8b181c3afe4c62cb20ae540270a93ede42f
Trevor Bright <t0brig01@louisville.edu>
2020-11-28 14:35:12 -0500

    base shit

--- myapp.py
+++ myapp.py
@@ -6,14 +6,15 @@ Created on Sat Nov 21 15:08:30 2020
 """
 
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from ypstruct import structure
 import ga
 
 #squere test function
 def sphere(x):
-    # return sum((np.e**(-2*np.log(2)*(((x-0.08)/0.854)**2)))*(np.sin(5*np.pi*((x**.75)-.05))**6)) This one breaks it idk why
-    return sum(np.power(np.sin(5*np.pi*x),6))
+    return sum(np.power(np.e,-2*np.log(2)*np.power((x-0.08)/0.854,2))*np.power(np.sin(5*np.pi*((np.sign(x)*(np.abs(x))**.75)-.05)),6))    
+    # return sum(np.power(np.sin(5*np.pi*x),6))
     # return sum(x**2)  #cost function
 
 # Problem Definition
